275eedfdc7c6c1a7e839d7590706e180b657987b
ZxStim <thang.victor.luong@gmail.com>
2025-05-08 12:05:34 +0700

    fix: added loading page

---
+++ app/explore-ideas/loading.tsx
@@ -0,0 +1,12 @@
+import GoBack from "@/components/go-back";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Loading() {
+  return (
+    <main className="flex flex-col gap-4 p-4">
+      <GoBack />
+      <h1 className="text-2xl font-bold">Explore Ideas</h1>
+      <Skeleton className="h-[400px] w-full" />
+    </main>
+  );
+}
