42838c24a5ea159f047d05160f5b36cb1a59128c
ZxStim <thang.victor.luong@gmail.com>
2025-05-08 11:53:21 +0700

    feat: added some footer contents

--- app/grants/page.tsx
+++ app/grants/page.tsx
@@ -1,3 +1,10 @@
 export default function Grants() {
-  return <div>Grants</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Grants</h1>
+      <p className="text-sm text-muted-foreground">
+        Polkadot Builders Grants are a way to support the Polkadot ecosystem.
+      </p>
+    </div>
+  );
 }
