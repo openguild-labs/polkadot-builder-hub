3a3bb971f363da71124d88326617add9cd95cafb
ZxStim <thang.victor.luong@gmail.com>
2025-05-16 23:48:08 +0700

    fix: edit grant card

--- components/grant-card.tsx
+++ components/grant-card.tsx
@@ -5,10 +5,12 @@ import { Button } from "@/components/ui/button";
 
 export default function GrantCard({ grant }: { grant: Grant }) {
   return (
-    <div className="flex flex-col gap-2 border-2 border-muted rounded-lg p-4 transition-all duration-200 hover:-translate-y-1 hover:shadow-lg hover:shadow-foreground/10">
-      <h2 className="text-lg font-bold">{grant.title}</h2>
-      <p className="text-sm text-muted-foreground">{grant.description}</p>
-      <Button asChild className="hover:cursor-pointer self-end">
+    <div className="flex flex-col gap-2 border-2 border-muted rounded-lg p-4 transition-all duration-200 hover:-translate-y-1 hover:shadow-lg hover:shadow-foreground/10 justify-between">
+      <div>
+        <h2 className="text-lg font-bold">{grant.title}</h2>
+        <p className="text-sm text-muted-foreground">{grant.description}</p>
+      </div>
+      <Button asChild className="hover:cursor-pointer self-end mt-auto">
         <Link href={grant.url} target="_blank">
           Apply
           <ExternalLink />
--- components/mocks/grants.ts
+++ components/mocks/grants.ts
@@ -9,37 +9,37 @@ export const grants: Grant[] = [
   {
     id: 1,
     title: "Web3 Foundation Grant",
-    description: "Grant 1 description",
+    description: "a comprehensive grants program focused on funding software development and research efforts related to Polkadot and Kusama",
     url: "https://grants.web3.foundation/docs/process"
   },
   {
     id: 2,
     title: "Fast Grant Bounty",
-    description: "Grant 1 description",
+    description: "The Polkadot Fast-Grants Programme provides funding for early-stage builders on Polkadot",
     url: "https://github.com/Polkadot-Fast-Grants/apply",
   },
   {
     id: 3,
     title: "OpenSource Bounty",
-    description: "Grant 1 description",
+    description: "The Polkadot Open Source Developer Grants bounty supports individuals and small teams in developing open-source software for the Polkadot ecosystem",
     url: "https://github.com/PolkadotOpenSourceGrants/apply",
   },
   {
     id: 4,
     title: "OpenGuild Meetup for Builders",
-    description: "Grant 1 description",
+    description: "Get funding to host a Polkadot Mini Meetup in your city within South East Asia",
     url: "https://docs.google.com/forms/d/e/1FAIpQLSdRw9EMCoe4N8iA_akQbiLBCh_L8GDyqBeNEsC87gv3AXEn4Q/viewform",
   },
   {
     id: 5,
     title: "Event Bounty",
-    description: "Grant 1 description",
+    description: "Get funding to host a Polkadot event",
     url: "https://forms.monday.com/forms/6333f358de606dd1eded5a06d478db1a?r=euc1",
   },
   {
     id: 6,
     title: "Meetup Bounty",
-    description: "Grant 1 description",
+    description: "Get funding to host a Polkadot Meetup",
     url: "https://dotmeetups.notion.site/how-to-start",
   },
 ];
