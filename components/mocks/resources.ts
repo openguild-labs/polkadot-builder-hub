2861d3ad7a5ccec7f96c36c669767d1096a86285
ZxStim <thang.victor.luong@gmail.com>
2025-05-23 14:28:03 +0700

    feat: sub pages in hackathon resources

--- components/mocks/resources.ts
+++ components/mocks/resources.ts
@@ -2,6 +2,7 @@ export type Resource = {
   id: number;
   title: string;
   description: string;
+  path: string;
 };
 
 export const resources: Resource[] = [
@@ -9,35 +10,42 @@ export const resources: Resource[] = [
     id: 1,
     title: "Tips to win hackathon on Polkadot",
     description: "Learn how to win hackathon on Polkadot",
+    path: "/hackathon-resources",
   },
   {
     id: 2,
     title: "Getting grants on Polkadot",
     description: "Learn how to win hackathon on Polkadot",
+    path: "/grants",
   },
   {
     id: 3,
     title: "Polkadot Developers Resources",
     description: "Learn how to get started on Polkadot",
+    path: "/developers",
   },
   {
     id: 4,
     title: "Teammate Finder Directory (coming soon)",
     description: "Learn how to get started on Polkadot",
+    path: "/teammate-finder",
   },
   {
     id: 5,
     title: "Event Calendar",
     description: "Learn how to get started on Polkadot",
+    path: "/events",
   },
   {
     id: 6,
     title: "Learn before Build",
     description: "Learn how to get started on Polkadot",
+    path: "/learn-before-build",
   },
   {
     id: 7,
     title: "Pitchdeck Template",
     description: "Learn how to get started on Polkadot",
+    path: "/pitchdeck-template",
   },
 ];
--- components/resource-card.tsx
+++ components/resource-card.tsx
@@ -1,9 +1,21 @@
-import type { Resource } from "./mocks/resources";
+import type { Resource } from "@/components/mocks/resources";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function ResourceCard({ resource }: { resource: Resource }) {
   return (
-    <div className="border-2 border-muted rounded-lg p-4">
-      <h2 className="text-lg font-bold">{resource.title}</h2>
+    <div className="flex flex-col gap-2 border-2 border-muted rounded-lg p-4 transition-all duration-200 hover:-translate-y-1 hover:shadow-lg hover:shadow-foreground/10 justify-between">
+      <div>
+        <h2 className="text-lg font-bold">{resource.title}</h2>
+        <p className="text-sm text-muted-foreground">{resource.description}</p>
+      </div>
+      <Button asChild className="hover:cursor-pointer self-end mt-auto">
+        <Link href={resource.path}>
+          View
+          <ArrowRight />
+        </Link>
+      </Button>
     </div>
   );
 }
