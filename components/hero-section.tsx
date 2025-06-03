ad8400fc091b771f2d358bcf79b81d30030412db
ZxStim <thang.victor.luong@gmail.com>
2025-06-03 16:47:33 +0700

    feat: added new background

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,32 +1,27 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function HeroSection() {
   return (
     <div 
-      className="flex flex-col gap-4 text-center items-center justify-center h-[700px] bg-gradient-to-br from-cyan-100/80 via-blue-100/80 to-purple-400/80 relative"
-      style={{
-        backgroundImage: 'url("/polkadot-builders-bg.png")',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat'
-      }}
+      className="flex flex-col gap-4 text-center items-center justify-center h-[700px] relative"
     >
-      <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-white" />
-      <div className="flex flex-col gap-4 relative z-10">
-        <h1 className="text-4xl font-bold">Polkadot Builders Resources</h1>
+      <div className="flex flex-col gap-4 relative z-10 items-center bg-secondary/80 p-24 rounded-4xl">
+        <Image src="/polkadot.svg" alt="Polkadot logo" width={300} height={300} />
+        <h1 className="text-6xl font-medium">Builders Resources</h1>
         <p className="text-lg">
           Aggregated by OpenGuild ♥️
         </p>
-        <p className="text-lg font-semibold">
+        <p className="text-lg font-semibold mt-12">
           Start building on Polkadot Hub - powerful smart contract layers for
           consumer dApps.
         </p>
-        <div className="flex flex-row gap-4 justify-center mt-4">
-          <Button className="bg-primary hover:bg-[#FF2670] hover:text-white transition-all duration-200 ease-in-out" asChild>
+        <div className="flex flex-col md:flex-row gap-4 justify-center mt-4">
+          <Button className="bg-primary hover:bg-[#FF2670] hover:text-white transition-all duration-200 ease-in-out px-12 py-6 text-xl" asChild>
             <Link href="/explore-ideas?category=all">Explore ideas</Link>
           </Button>
-          <Button variant="secondary" className="hover:cursor-pointer" asChild>
+          <Button variant="secondary" className="hover:cursor-pointer px-12 py-6 text-xl" asChild>
             <Link href="/hackathon-resources">Hackathon resources</Link>
           </Button>
         </div>
