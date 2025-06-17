4d792c462717a8cc002ff13e2b2714af94840423
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-06-17 17:43:14 +0700

    chore: add trusted origin

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db"; // your drizzle instance
 import { user, account, session, verification } from "@/db/schema/auth-schema";
-import { admin } from "better-auth/plugins"
+import { admin } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -26,8 +26,11 @@ export const auth = betterAuth({
       },
     },
   },
-  trustedOrigins: ["http://localhost:3080", "https://polakdot-builders.vercel.app"],
-  plugins: [
-    admin(),
+  trustedOrigins: [
+    "http://localhost:3080",
+    "https://polakdot-builders.vercel.app",
+    "https://build.openguild.wtf",
+    "https://www.polkadotbuilders.com",
   ],
+  plugins: [admin()],
 });
