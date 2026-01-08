cd9287129d4dc2e4cbf817f6985f0c56d66583c9
chungquantin <cqtin0903@gmail.com>
2026-01-08 15:15:50 +0700

    chore: update the builder hub banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,33 +5,33 @@ import Footer from "@/components/footer";
 import Providers from "@/app/providers";
 
 export const metadata: Metadata = {
-  title: 'Build on Polkadot',
-  description: 'A collection of all resources for builders on Polkadot',
-  metadataBase: new URL('https://www.polkadotbuilders.com'),
+  title: "Build on Polkadot",
+  description: "A collection of all resources for builders on Polkadot",
+  metadataBase: new URL("https://www.polkadotbuilders.com"),
   openGraph: {
-    title: 'Build on Polkadot',
-    description: 'A collection of all resources for builders on Polkadot',
-    url: 'https://www.polkadotbuilders.com',
-    siteName: 'Build on Polkadot',
+    title: "Build on Polkadot",
+    description: "A collection of all resources for builders on Polkadot",
+    url: "https://www.polkadotbuilders.com",
+    siteName: "Build on Polkadot",
     images: [
       {
-        url: '/polkadot-builders-tbn.png',
+        url: "/og-banner.png",
         width: 1200,
         height: 630,
-        alt: 'og-image',
+        alt: "Build on Polkadot - Builders Hub",
       },
     ],
-    locale: 'en_US',
-    type: 'website',
+    locale: "en_US",
+    type: "website",
   },
   twitter: {
-    card: 'summary_large_image',
-    title: 'Build on Polkadot',
-    description: 'A collection of all resources for builders on Polkadot',
-    creator: '@polkadotbuilders',
-    images: ['/polkadot-builders-tbn.png'],
+    card: "summary_large_image",
+    title: "Build on Polkadot",
+    description: "A collection of all resources for builders on Polkadot",
+    creator: "@polkadotbuilders",
+    images: ["/og-banner.png"],
   },
-}
+};
 
 export default function RootLayout({
   children,
